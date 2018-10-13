18062067de2b5ba1c17cd50b88af831cd7367a33
unknown <nikohartline@gmail.com>
2018-10-12 23:38:03 -0700

    more setup of pi environment

---
+++ pi_functions.py
@@ -0,0 +1,7 @@
+# helpul functions for various tasks
+
+import pandas as pd
+import numpy as np
+
+def hello_world():
+	print("hello_world")
--- projects/email_graph_generator/hello_world.py
+++ projects/email_graph_generator/hello_world.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python
 
+import sys
+
+sys.path.insert(0,'/p3/hartline-pi')
+import pi_functions as pf
+
 print('hello world!')
+
+pf.hello_world()
